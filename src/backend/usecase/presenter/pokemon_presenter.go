9f895d1c9df30c62549cf8c234b7171a24f400de
GerardoHP <gerardo.hernandez@wizeline.com>
2022-04-08 16:21:43 -0500

    Adding frontend app and changing names accordingly

--- src/backend/usecase/presenter/pokemon_presenter.go
+++
@@ -1,7 +0,0 @@
-package presenter
-
-import "github.com/GerardoHP/ondemand-go-bootcamp/domain/model"
-
-type PokemonPresenter interface {
-	ResponsePresenter(p []*model.Pokemon) []*model.Pokemon
-}
--- src/backend/usecase/repository/pokemon_repository.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import "github.com/GerardoHP/ondemand-go-bootcamp/domain/model"
-
-type PokemonRepositoty interface {
-	FindAll(p []*model.Pokemon) ([]*model.Pokemon, error)
-}
