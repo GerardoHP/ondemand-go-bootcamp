5aa24a6e91dddfb0ec4613ebb16cffac1e982835
Gerardo Hernandez <gerardojhp@gmail.com>
2022-05-22 15:29:05 -0500

    Working concept

--- src/pokemon-service/utils/file.go
+++ src/pokemon-service/utils/file.go
@@ -69,68 +69,59 @@ func (f file) AppendLineToFile(line string) error {
 }
 
 func (f file) ReadAllFileConcurrent(even bool, items, items_per_worker int) ([]string, error) {
-	const workers = 1
+	const workers = 2
 	wg := sync.WaitGroup{}
 	linesCh := make(chan string)
-	pokemonCount := make(chan int)
-	mt := sync.RWMutex{}
-	file, err := os.Open(f.filename)
-	if err != nil {
-		log.Panic("Failed to open", err)
-		return nil, err
-	}
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	wg.Add(workers)
-	for i := 0; i < workers; i++ {
-		go readLine(scanner, even, items, items_per_worker, linesCh, pokemonCount, &wg, &mt)
+	var processedLines []string
+	mt := sync.Mutex{}
+	wg.Add(2)
+	for i := 0; i < 2; i++ {
+		go processLines(linesCh, &processedLines, &wg, &mt, even, items, items_per_worker)
 	}
 
 	wg.Add(1)
-	go coordinateChan(&lines, &wg, linesCh, pokemonCount)
-	if err = scanner.Err(); err != nil {
-		return nil, err
-	}
+	go processFile(f.filename, &wg, linesCh)
 
 	wg.Wait()
-	return lines, nil
+	return processedLines, nil
 }
 
-func coordinateChan(lines *[]string, wg *sync.WaitGroup, linesCh <-chan string, pokemonCount chan<- int) {
+func processFile(fileName string, wg *sync.WaitGroup, lines chan<- string) {
 	defer wg.Done()
-	for line := range linesCh {
-		*lines = append(*lines, line)
-		pokemonCount <- len(*lines)
+	file, _ := os.Open(fileName)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines <- scanner.Text()
 	}
-
-	close(pokemonCount)
+	// if err = scanner.Err(); err != nil {
+	// 	return nil, err
+	// }
+	close(lines)
 }
 
-func readLine(s *bufio.Scanner, even bool, items, items_per_worker int, linesCh chan<- string, pokemonCount <-chan int, wg *sync.WaitGroup, mt *sync.RWMutex) {
+func processLines(lines <-chan string, processedLines *[]string, wg *sync.WaitGroup, mt *sync.Mutex, even bool, items, items_per_worker int) {
 	defer wg.Done()
 	counter := 0
-	for s.Scan() {
-		mt.RLock()
-		line := s.Text()
-		mt.RUnlock()
-		lineDesc := strings.Split(line, ",")
-		id, err := strconv.Atoi(lineDesc[0])
+	for line := range lines {
+		args := strings.Split(line, ",")
+		id, err := strconv.Atoi(args[0])
 		if err != nil {
-			log.Print("failed to convert id")
-			continue
+			log.Print("error converting id: ", args[0])
 		}
 
 		isEven := id%2 == 0
-		nPok := <-pokemonCount
 		if isEven == even {
-			linesCh <- line
-			counter++
-			if counter == items_per_worker || nPok > items {
-				break
+			mt.Lock()
+			currentCount := len(*processedLines)
+			mt.Unlock()
+			if counter >= items_per_worker || currentCount >= items {
+				continue
 			}
+
+			mt.Lock()
+			*processedLines = append(*processedLines, line)
+			mt.Unlock()
+			counter++
 		}
 	}
-
-	close(linesCh)
 }
